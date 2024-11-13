902560293c076d06f2c3b9d1a719010118a19515
servetisikli <servetisikli@gmail.com>
2024-11-13 21:51:10 +0100

    updated

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -30,8 +30,8 @@ const ChatRoom = () => {
   }, [messages]);
 
   return (
-    <div className="flex flex-col h-screen bg-gray-100 py-4">
-      <div className="flex flex-col flex-grow overflow-y-auto p-4 bg-white shadow-md w-full max-w-6xl mx-auto mb-8 rounded-md sm:rounded-lg">
+    <div className="flex flex-col h-screen w-screen bg-gray-100 items-center justify-center">
+      <div className="flex flex-col flex-grow overflow-y-auto p-4 bg-white shadow-md w-full max-w-4xl mx-auto mb-0 rounded-none sm:rounded-lg">
         <h2 className="text-xl sm:text-2xl font-bold mb-4 text-center sm:text-left">
           Real-Time Chat
         </h2>
@@ -46,7 +46,7 @@ const ChatRoom = () => {
               <span className="font-bold block text-sm sm:text-base mb-1">
                 {msg.username}:
               </span>
-              <span className="block text-xs sm:text-sm md:text-base">
+              <span className="block text-xs sm:text-sm md:text-base break-words">
                 {msg.message}
               </span>
             </li>
